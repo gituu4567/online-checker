5b4802dbe96ac4f177bd80b10cabe569612ae1de
abc <psroadheaven@gmail.com>
2017-07-15 15:20:14 +0530

    pr

---
+++ runthis.py
@@ -0,0 +1,37 @@
+
+# *****************************************************
+# whatsapp scripting v1.0
+# Author:
+# Parwinder <psroadheaven@gmail.com>
+# https://github.com/parwinders
+# 3oth march 2017 
+# *****************************************************
+
+import os;os.system("cls")
+import wa
+import sys
+#import ipdb;ipdb.set_trace()
+
+
+wa.whatsapp().start()
+wa.whatsapp().load()
+wa.whatsapp().postcookie()
+
+try:
+    target = "mnisha,saran"
+    #target = raw_input('enter contacts \",\" seperated  :')
+    target = wa.whatsapp().cleanlist(target)       
+    print target
+
+    while (wa.whatsapp().loggedin()==False):
+            wa.whatsapp().loggedin()
+            
+    while True:
+            for i in range(len(target)):
+                    contact, status = wa.whatsapp().lastseen(target[i])
+                    print "\n%s      ==> %s  ==> %s " %(target[i], contact, status)
+
+except KeyboardInterrupt:
+    print "\nbye"
+    wa.webdriver.quit()
+    sys.exit(0)
